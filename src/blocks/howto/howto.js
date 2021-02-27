3df10d68144dd0daa24fb2fb0efab5dd8b2a5abf
castetus <kboikov@mail.ru>
2021-02-28 00:25:06 +0300

    feat: finish markup

---
+++ src/blocks/howto/howto.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const howtoSlider = new Swiper('.howto', {
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+  })
+})
---
+++ src/blocks/opps/opps.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const width = document.documentElement.clientWidth
+  if (width >= 768){
+    const slides = document.querySelectorAll('.opps__item')
+    for (let elem of slides){
+      elem.classList.remove('swiper-slide')
+    }
+    return
+  }
+  const oppsSlider = new Swiper('.opps__slider', {
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+  })
+})
